1d96d8ed81382743006f668c4bfc66326ec1836e
Vojtech Kurka <vojtech.kurka@gmail.com>
2015-09-01 21:43:37 +0200

    Update ab.sh

--- console/ab.sh
+++ console/ab.sh
@@ -1,2 +1,5 @@
 ab --help
 ab -n 100 -c 5 http://codecamp.cz/
+
+# 4 parallel requests, 2K requests total, gzip
+ab -c4 -n2000  -k -H 'Accept-Encoding: gzip'  -H 'User-Agent: XYZbot' "http://www.glami.cz/"
